40b7e3ca0ec723aa0b695b0aa76a816e0baae7ec
Kevin Song <song_zj@hotmail.com>
2023-07-17 23:26:09 -0500

    begin data analysis

---
+++ helloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+
+    int num = 0;
+    char letter = 'a';
+    printf("%d", num);
+    printf("%c", letter);
+
+    printf("My favorite number is %d and my favorite letter is %c\n", num, letter);
+
+    return 0;
+}
